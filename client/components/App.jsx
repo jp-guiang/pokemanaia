deb6f86cd0b83e32e5a4209e7dcf14949bbbfe2b
Aaron <95986810+Aaront028@users.noreply.github.com>
2022-09-21 15:09:00 +1200

    phaser and react map implementation

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
-import Fruits from './Fruits'
-
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-`
 
 const App = () => {
   return (
-    <Container>
-      <h1>I Love Fruit!</h1>
-      <Fruits />
-    </Container>
+    <div>
+      <h1>Pokemon</h1>
+    </div>
   )
 }
 
