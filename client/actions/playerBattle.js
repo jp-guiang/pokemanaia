5b65c38b84e8357766458b8fb2a25fb4d9a3e5c6
Veeta Orinko <107618223+veeta-orinko@users.noreply.github.com>
2022-09-22 12:11:15 +1200

    added battle route

--- client/actions/playerBattle.js
+++
@@ -1,9 +0,0 @@
-export const SET_MY_POKEMON = 'SET_MY_POKEMON'
-export const MY_POKEMON_HP = 'MY_POKEMON_HP'
-
-export function setCurrentPokemon(payload) {
-  return {
-    type: 'SET_MY_POKEMON',
-    payload,
-  }
-}
