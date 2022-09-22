97dfd9741408c61ddb67022f022967a22d436cd2
ash-fitzgibbon <107602601+ash-fitzgibbon@users.noreply.github.com>
2022-09-23 11:24:49 +1200

    map fixed world in progress ,collision added to upper layer etc

---
+++ server/public/big tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="big tileset" tilewidth="32" tileheight="32" tilecount="5208" columns="8">
+ <image source="big tileset.png" width="256" height="20832"/>
+</tileset>
