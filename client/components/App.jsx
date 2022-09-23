ccfd99dc3dbf5fe4f7148f54c7cfd7789bce8971
Eli Bing <107372513+elizabeth-bing@users.noreply.github.com>
2022-09-23 14:46:36 +1200

    clean code

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,20 +9,16 @@ const App = () => {
   const [showWorld, setShowWorld] = useState(false)
 
   function aWholeNewWorld(event) {
-    // event.preventDefault()
     setShowWorld(!showWorld)
     console.log('a whole new world')
   }
   return (
     <div>
-      {/* <button onClick={aWholeNewWorld}>Confirm</button> */}
       <h1>Choose your Pokemon!</h1>
       {!showWorld && <Pokemon fn={aWholeNewWorld} />}
       {showWorld && <World />}
-      {/* {showWorld && <p>TESTERSAERGAAG</p>} */}
       <h1>Pokemon</h1>
 
-      {/* commenting these out for now cause pc breaky -jp */}
       {/* <Battle />
       <World /> */}
     </div>
