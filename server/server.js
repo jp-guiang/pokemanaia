d2bb53656cb41c6d99116264dad9937aa661ca26
Max (Amber) Hilton <107532278+max-is-coding@users.noreply.github.com>
2022-09-27 18:11:56 +1300

    ughghghggh

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const path = require('path')
 
-const teamRoutes = require('./routes/teamHistory')
-
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
-
-server.use('/api/v1/teamHistory', teamRoutes)
 
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
